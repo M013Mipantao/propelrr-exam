9bde4a3c76f096078362f5eced353b73ad592de6
Mohammad Michael Mipantao <mohammad.mipantao@30kft.com.au>
2023-04-16 17:48:25 +0800

    Add the required validation

--- Sign-up/controller/validate.php
+++ Sign-up/controller/validate.php
@@ -3,14 +3,39 @@
 $errors = [];
 $data = [];
 
-if (empty($_POST['name'])) {
-    $errors['name'] = 'Name is required.';
+if (!empty($_POST['fullname'])) {
+    if(!preg_match('/^[a-zA-Z\s.,]+$/', $_POST['fullname'])){
+        $errors['fullname'] = 'Text and characters like comma and period only';
+    }
+}else{
+    $errors['fullname'] = 'Name is required.';
 }
 
-if (empty($_POST['email'])) {
+if (!empty($_POST['email'])) {
+    if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'Check the email format';
+    }
+}else{
     $errors['email'] = 'Email is required.';
 }
 
+if (!empty($_POST['number'])) {
+    if(!preg_match('/^[0-9]{11}+$/', $_POST['number'])){
+        $errors['number'] = 'Incorrect mobile number format';
+    }
+}else{
+    $errors['number'] = 'Mobile Number is required.';
+}
+
+if (empty($_POST['dateofbirth'])) {
+    $errors['dateofbirth'] = 'Date of Birth is required.';
+}
+if (empty($_POST['age'])) {
+    $errors['age'] = 'Age is required.';
+}
+if (empty($_POST['gender'])) {
+    $errors['gender'] = 'Gender is required.';
+}
 
 if (!empty($errors)) {
     $data['success'] = false;
--- Sign-up/js/validate.js
+++ Sign-up/js/validate.js
@@ -1,19 +1,79 @@
 $(document).ready(function () {
     $("form").submit(function (event) {
+
+        $(".form-group").removeClass("has-error");
+        $(".help-block").remove();
+
       var formData = {
-        name: $("#name").val(),
+        fullname: $("#fullname").val(),
         email: $("#email").val(),
-        superheroAlias: $("#superheroAlias").val(),
+        number: $("#number").val(),
+        dateofbirth: $('#date_picker').val(),
+        age: $('#age').val(),
+        gender: $('#gender').val()
       };
   
       $.ajax({
         type: "POST",
-        url: "process.php",
+        url: "controller/validate.php",
         data: formData,
         dataType: "json",
         encode: true,
       }).done(function (data) {
         console.log(data);
+        if (!data.success) {
+
+            if (data.errors.fullname) {
+                $("#fullname-group").addClass("has-error");
+                $("#fullname-group").append(
+                    '<div class="help-block">' + data.errors.fullname + "</div>"
+                );
+            }
+
+            if (data.errors.email) {
+                $("#email-group").addClass("has-error");
+                $("#email-group").append(
+                '<div class="help-block">' + data.errors.email + "</div>"
+                );
+            }
+
+            if (data.errors.number) {
+                $("#number-group").addClass("has-error");
+                $("#number-group").append(
+                '<div class="help-block">' + data.errors.number + "</div>"
+                );
+            }
+
+            if (data.errors.dateofbirth) {
+                $("#dateofbirth-group").addClass("has-error");
+                $("#dateofbirth-group").append(
+                '<div class="help-block">' + data.errors.dateofbirth + "</div>"
+                );
+            }
+
+            if (data.errors.age) {
+                $("#age-group").addClass("has-error");
+                $("#age-group").append(
+                '<div class="help-block">' + data.errors.age + "</div>"
+                );
+            }
+
+            if (data.errors.gender) {
+                $("#gender-group").addClass("has-error");
+                $("#gender-group").append(
+                '<div class="help-block">' + data.errors.gender + "</div>"
+                );
+            }
+
+        } else {
+            $("form").html(
+            '<div class="alert alert-success">' + data.message + "</div>"
+            );
+        }
+      }).fail(function (data) {
+        $("form").html(
+          '<div class="alert alert-danger">Could not reach server, please try again later.</div>'
+        );
       });
   
       event.preventDefault();
